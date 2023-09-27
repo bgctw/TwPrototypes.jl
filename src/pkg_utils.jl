ed03d5e5cce73f5a6a9e7b6ab13f51a59f7016f8
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2023-09-27 08:28:32 +0200

    simplify makeddocs

--- src/pkg_utils.jl
+++ src/pkg_utils.jl
@@ -1,24 +1,19 @@
 """
     makeddocs()
 
-Execute docs/make.jl in an environment that includes the current project.
+include docs/make.jl in the docs environment which devs the current project.
 
 By copying the docs/Project, activating it, adding the current project
 and finally activating the environment used before.
-
-This helps with the following issue:
-
-- When running include("docs/make.jl") from the package environment, the package dependencies of the docs project are not checked.
-- When running from docs environment, the dependency on the package under development is not included.
 """
 function makeddocs() 
-    path = mkpath("tmp/docs_project")
-    cp("docs/Project.toml", joinpath(path,"Project.toml"), force=true)
+    path = "docs"
     prev_path = Pkg.project().path
     try
         Pkg.activate(path)
         @show pwd()
         Pkg.develop(path=".")
+        #Pkg.instantiate()
         Base.include(Main, "docs/make.jl")
     finally
         Pkg.activate(prev_path)
