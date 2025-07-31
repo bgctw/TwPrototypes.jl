b12373844b25867253d77fce5d5a9646c6233400
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2025-07-31 20:37:06 +0200

    remove Requires, bump min versions

--- src/TwPrototypes.jl
+++ src/TwPrototypes.jl
@@ -44,25 +44,6 @@ include("util_slurm.jl")
 export downgrade_project
 include("version_util.jl")
 
-if !isdefined(Base, :get_extension)
-    using Requires
-end
 
-@static if !isdefined(Base, :get_extension)
-    function __init__()
-        @require CairoMakie = "13f3f980-e62b-5c42-98c6-ff1f3baf88f0" begin
-            include("../ext/TwPrototypesCairoMakieExt.jl")
-            @require AlgebraOfGraphics = "cbdf2221-f076-402e-a563-3d30da359d67" begin
-                include("../ext/TwPrototypesAoGExt.jl")
-            end
-        end
-        @require MCMCChains = "c7f686f2-ff18-58e9-bc7b-31028e88f75d" begin
-            include("../ext/TwPrototypesMCMCChainsExt.jl")
-        end
-    end
-end
-
-
-
-    
+   
 end # module
