476b4427d0e638f53cbd8c875af71e51b625d06d
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2026-08-19 14:01:42 +0200

    add missing compats in test and docs Project.toml and bump dev version (#24)
    * add missing compats in test and docs Project.toml and bump dev version

--- scripts/compat_helpers.jl
+++ scripts/compat_helpers.jl
@@ -25,14 +25,28 @@ project_file = Pkg.project().path
 project = TOML.parsefile(project_file)
 existing_compat = get(project, "compat", Dict())
 
+# Look for name in current project, or fall back to parent project
+project_name = get(project, "name", nothing)
+if project_name === nothing
+    parent_project_file = joinpath(dirname(project_file), "..", "Project.toml")
+    if isfile(parent_project_file)
+        parent_project = TOML.parsefile(parent_project_file)
+        project_name = get(parent_project, "name", nothing)
+    end
+end
+
 for (_, dep) in Pkg.dependencies()
     dep.is_direct_dep || continue
+    # if dep.name == "Statistics" 
+    #     Main.@infiltrate_main
+    # end
+    dep.name == project_name && continue  # skip the main package
     spec = version_spec(dep.version)
     
     if !haskey(existing_compat, dep.name)
         Pkg.compat(dep.name, spec)
     else
-        existing_spec = Pkg.Version.semver_spec(existing_compat[dep.name])
+        existing_spec = Pkg.Versions.semver_spec(existing_compat[dep.name])
         if !in(dep.version, existing_spec)
             new_compat = existing_compat[dep.name] * ", " * spec
             Pkg.compat(dep.name, new_compat)
